daea24c2adc747856a7f27b8a3a2635f51e2f0a5
Shahzad Tariq <shahzad.tariq92@gmail.com>
2023-11-15 13:23:33 +0800

    Build a responsive layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  // In Show component, we put aside which we will hide on small screns.
+  // Whatever we will put in Show component will be render on large screens.
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // Wider then 1024 pixels
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
